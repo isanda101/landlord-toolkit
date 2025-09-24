2a1b48e04939816bf2a92544e9a8262774d3ad25
Ibrahim Sanda <ibrahimsanda@Ibrahims-MacBook-Air-M2.local>
2025-09-24 09:21:33 -0400

    Serve static robots.txt and sitemap.xml from /public

--- app/robots.js
+++
@@ -1,7 +0,0 @@
-export default function robots() {
-  return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://landlord-toolkit.vercel.app/sitemap.xml',
-    host: 'landlord-toolkit.vercel.app',
-  };
-}
