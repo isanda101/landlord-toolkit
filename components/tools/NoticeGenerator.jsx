0122dadac5ab0f6c043ffaba66643ad8f0cff01b
Ibrahim Sanda <ibrahimsanda@Ibrahims-MacBook-Air-M2.local>
2025-10-09 13:30:29 -0400

    chore: make '@/components/tools' export new ClientPages (prorated, deposit) so Home matches routes

---
+++ components/tools/NoticeGenerator.jsx
@@ -0,0 +1,9 @@
+"use client";
+export default function NoticeGenerator() {
+  return (
+    <div className="rounded-xl border p-4">
+      <div className="font-semibold mb-2">Notice Generator</div>
+      <p className="text-sm text-gray-600">Notice Generator component not found. Replace this placeholder with your existing component.</p>
+    </div>
+  );
+}
---
+++ components/tools/index.js
@@ -0,0 +1,3 @@
+export { default as ProratedRent } from "./ProratedRent";
+export { default as DepositTracker } from "./DepositTracker";
+export { default as NoticeGenerator } from "./NoticeGenerator";
